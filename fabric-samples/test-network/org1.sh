31a643edd4925413608791d38b3f40a247cd9cb6
Tal Derei <tad222@lehigh.edu>
2022-02-06 17:54:17 -0500

    added environment variables

---
+++ fabric-samples/test-network/org1.sh
@@ -0,0 +1,7 @@
+export FABRIC_CFG_PATH=$HOME/Hyperledger-Fabric-DCLF/fabric-samples/config
+export CHANNEL_NAME="mychannel"
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=$HOME/Hyperledger-Fabric-DCLF/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=$HOME/Hyperledger-Fabric-DCLF/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
